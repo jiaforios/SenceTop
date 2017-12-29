faebb584730ec88ffeb65f1f7c41f9d7f054ee1d
jiaforios <jiaforios@163.com>
2017-12-29 16:33:16 +0800

    安卓实例

---
+++ app/src/main/java/sence/com/sencetop/CustomApplication.java
@@ -0,0 +1,16 @@
+package sence.com.sencetop;
+
+import android.app.Application;
+
+/**
+ * Created by admina on 2017/12/29.
+ */
+public class CustomApplication extends Application{
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+
+}
---
+++ app/src/main/java/sence/com/sencetop/MainActivity.java
@@ -0,0 +1,13 @@
+package sence.com.sencetop;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
